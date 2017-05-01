001ccb4db7d8c35e2b74e3316a6872b65bc2de90
vagrant <vagrant@homestead>
2017-05-01 16:25:32 +0000

    Theme setup

---
+++ wp-content/themes/akcija-mladih/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package akcija-mladih
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'akcija-mladih' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'akcija-mladih' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'akcija-mladih' ), 'akcija-mladih', '<a href="https://automattic.com/" rel="designer">Alen Barac</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
